7b50a0e45c7bedab82201d6133ab0e33c8852107
VeronikaSemyonova <veronikase260996@mail.ru>
2016-03-03 13:13:59 +0500

    Пробная версия

---
+++ проба/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Form2 Form2 = new Form2();
+            this.Hide();
+            Form2.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form3 Menu = new Form3();
+            this.Hide();
+            Menu.Show();
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            Form4 Fon = new Form4();
+            this.Hide();
+            Fon.Show();
+        }
+    }
+}
---
+++ проба/WindowsFormsApplication1/Form4.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.BackgroundImage = WindowsFormsApplication1.Properties.Resources.i;
+            this.BackgroundImageLayout = ImageLayout.Zoom;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.BackgroundImage = WindowsFormsApplication1.Properties.Resources.i1;
+            this.BackgroundImageLayout = ImageLayout.Zoom;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.BackgroundImage = WindowsFormsApplication1.Properties.Resources._1380692332_3126244_nevseoboi_com_ua;
+            this.BackgroundImageLayout = ImageLayout.Zoom;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.BackgroundImage = WindowsFormsApplication1.Properties.Resources.background_wide_wallpaper_1920x1080_005;
+            this.BackgroundImageLayout = ImageLayout.Zoom;
+        }
+    }
+}
